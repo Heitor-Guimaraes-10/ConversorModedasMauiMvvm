bd347dbad0c2b825698733822f31d22e04715f4a
Heitor.galmeida1 <Heitor.galmeida1@TIT0676440W11-1>
2025-09-17 14:28:35 -0300

    Adicionar arquivos de projeto.

---
+++ Models/RateTable.cs
@@ -0,0 +1,33 @@
+﻿namespace ConversorModedasMauiMvvm.Models
+{
+     public class RateTable
+    {
+        private readonly Dictionary<string, decimal>
+            _toBRL = new()
+            {
+                ["BRL"] = 1.00M,
+                ["USD"] = 5.60m,
+                ["EUR"] = 6.10m
+            };
+
+        public IReadOnlyDictionary<string, decimal>
+            ToBRL => _toBRL;
+
+        public IEnumerable<string> GetCurrencies()
+            => _toBRL.Keys.OrderBy(k => k);
+
+        public bool Supports(string code)
+            => _toBRL.ContainsKey(code);
+
+        public decimal Convert (decimal amount, string from, string to) 
+        {
+        if (!Supports(from)|| !Supports(to)) return 0m;
+        if (from == to) return amount;  
+
+
+        var brl = amount * _toBRL[from];
+
+            return brl / _toBRL[to];
+        }
+    }
+}
---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using ConversorModedasMauiMvvm.ViewModels;
+
+namespace ConversorModedasMauiMvvm
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainViewModel();
+        }
+    }
+}
